c02ef7bc9f8e6f9b0fa483c1c2a064b44ea6b4c3
Ryan Harvey <rharv99@gmail.com>
2024-04-25 00:39:43 -0400

    sync

--- includes/Constants.h
+++ includes/Constants.h
@@ -14,9 +14,10 @@ const int GAME_IS_RUNNING = 1;
 const float PI = 3.14159;
 
 // options
-const bool ALLOW_MOUSE_MOVEMENT = true;
-const bool ALLOW_MOUSE_SELECTION = true;
-const bool ALLOW_MOUSE_SHOOTING = true;
+const bool ALLOW_ALL_MOUSE = true;
+const bool ALLOW_MOUSE_MOVEMENT = false || ALLOW_ALL_MOUSE;
+const bool ALLOW_MOUSE_SELECTION = false || ALLOW_ALL_MOUSE;
+const bool ALLOW_MOUSE_SHOOTING = false || ALLOW_ALL_MOUSE;
 
 // for user's lasers
 const Color USER_LASER_COLOR = {255, 255, 0, 0};
